39c3819fe953a180f8470a4389aa7a63427eebb0
JoaoSampaio-26 <4541857722@estudante.sed.sc.gov.br>
2026-06-19 19:46:59 -0300

    add: adiciona projeto NetBeans

---
+++ ProjAutoHub/src/main/java/com/mycompany/projautohub/ProjAutoHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projautohub;
+
+/**
+ *
+ * @author Windows
+ */
+public class ProjAutoHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
